a687eee57a16fcaa4cd649e393a880e13973040f
AndyMcKenna <andy@shortskates.com>
2024-08-18 08:53:55 -0400

    Create sprite sheet and start the CSS classes

---
+++ src/views/TrackView.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <main>
+    
+    <p class="EnemyPool">
+      Prize Pack 1
+      <div class="icon BlueSwordGuard"></div>
+      <div class="icon GreenSwordGuard"></div>
+      <div class="icon BlueTarosu"></div>
+      <div class="icon BushStal"></div>
+      <div class="icon Crab"></div>
+      <div class="icon GreenDaggerGuard"></div>
+      <div class="icon GreenLeever"></div>
+      <div class="icon GreenSpearGuard"></div>
+      <div class="icon GreenZirro"></div>
+      <div class="icon Moblin"></div>
+      <div class="icon PurpleLeever"></div>
+      <div class="icon QuakeSlime"></div>
+      <div class="icon RedSpearShieldGuard"></div>
+      <div class="icon RedTarous"></div>
+      <div class="icon SkeletalWizzrobe"></div>
+      <div class="icon Snake"></div>
+      <div class="icon Toppo"></div>
+      <div class="icon Wizzrobe"></div>
+      <div class="clear"></div>
+    </p>
+    <p class="EnemyPool">Prize Pack 2</p>
+    <p class="EnemyPool">Prize Pack 3</p>
+    <p class="EnemyPool">Prize Pack 4</p>
+    <p class="EnemyPool">Prize Pack 5</p>
+    <p class="EnemyPool">Prize Pack 6</p>
+    <p class="EnemyPool">Prize Pack 7</p>
+
+    <p class="EnemyPool">Bush Crab</p>
+
+    <p class="EnemyPool">Stun Prize</p>
+
+    <p class="EnemyPool">Tree Pulls</p>
+
+    <p class="EnemyPool">Fish Prize</p>
+  </main>
+</template>
