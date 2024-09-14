69374b2bb79311c0ceb4c5804dfed1bae22b3561
AndyMcKenna <andy@shortskates.com>
2024-09-14 15:36:55 -0400

    The bomb in the small magic pack had the wrong class
    Fixes #10

--- src/components/prizes/packs/SmallMagicPack.vue
+++ src/components/prizes/packs/SmallMagicPack.vue
@@ -4,7 +4,7 @@
     <div class="icon Heart"></div>
     <div class="icon FiveArrows"></div>
     <div class="icon SmallMagic"></div>
-    <div class="icon SingleBomb"></div>
+    <div class="icon OneBomb"></div>
     <div class="icon GreenRupee"></div>
     <div class="icon Heart"></div>
 </template>
